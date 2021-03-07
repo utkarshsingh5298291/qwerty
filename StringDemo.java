71fe43ab703d41a09d32ad215925adf13abc0f97
Utkarsh Singh <42814916+utkarshsingh5298291@users.noreply.github.com>
2021-03-07 15:19:41 +0530

    Add files via upload

---
+++ StringDemo.java
@@ -0,0 +1,26 @@
+package Lecture5;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//Part-1
+		String str="Hello";
+		//str="bye";
+		System.out.println(str);
+		
+		//Part-2
+		System.out.println(str.charAt(0));
+		System.out.println(str.charAt(1));
+		System.out.println(str.charAt(2));
+		System.out.println(str.charAt(3));
+		System.out.println(str.charAt(4));
+		
+		System.out.println("Lenght:" +str.length());
+		System.out.println("last character:"+str.charAt(str.length()-1));
+		
+	}
+
+}
